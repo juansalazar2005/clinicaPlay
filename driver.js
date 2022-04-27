7695ead924c94bd01a9eb20ed78d6d2bf952ff2a
unknown <tipito4085@gmail.com>
2022-04-27 17:24:26 -0500

    Update+

---
+++ driver.js
@@ -0,0 +1,128 @@
+let producto1 = {
+    nombre: "Cable HDMI",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/12069774_1.jfif?alt=media&token=0aed271b-7961-4654-8e23-aa30bffeb7fd",
+    estado: "Nuevo",
+    precio: 15000
+}
+let producto2 = {
+    nombre: "Call Of Duty: Black Ops III",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/617JNobX2VL._SL1215_.jpg?alt=media&token=8ffedf9d-ffb7-447f-9ba0-b4cecfd6f258",
+    estado: "Nuevo",
+    precio: 75000
+}
+let producto3 = {
+    nombre: "Call Of Duty: Modern Warfare",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/81n2llGXyiL._SX342_.jpg?alt=media&token=6c60563a-9747-432b-b001-4166123315f4",
+    estado: "Nuevo",
+    precio: 75000
+}
+let producto4 = {
+    nombre: "FIFA 22",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61Kda%2BeUmlL._SL1000_.jpg?alt=media&token=67359025-390d-46d9-bfe2-3f14ccca7a07",
+    estado: "Nuevo",
+    precio: 150000
+}
+let producto5 = {
+    nombre: "Call Of Duty: Black Ops IIII",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61zWIw7-S3S._SX342_.jpg?alt=media&token=c39c21fd-dd94-4a8c-9eea-440ae0575b1f",
+    estado: "Nuevo",
+    precio: 80000
+}
+let producto6 = {
+    nombre: "DualShock 4",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/619FrDaqfAL._AC_SY355_.jpg?alt=media&token=79d05b17-5c57-42af-bcfe-69c5989ec422",
+    estado: "Nuevo",
+    precio: 150000
+}
+let producto7 = {
+    nombre: "Teclado",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/61Om-iwnAbL._AC_SY450_.jpg?alt=media&token=81c7f41c-a715-47c2-86ca-83a15db8c751",
+    estado: "Nuevo",
+    precio: 60000
+}
+let producto8 = {
+    nombre: "Cargador inalámbrico PS5",
+    capacidad: "2 Controles",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/9440503_2.jfif?alt=media&token=2c6b7aea-1be8-4be1-a4c9-49cb2d44ff56",
+    estado: "Nuevo",
+    precio: 125000
+}
+let producto9 = {
+    nombre: "Audífonos inalámbricos PS4",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/Aud%C3%ADfonos-Sony-Ps4-Gold-Wireless-Heasdet-Ps4.jpg?alt=media&token=2d4a44ae-0f15-4c65-9c24-ee4f6fff08a5",
+    estado: "Nuevo",
+    precio: 250000
+}
+let producto10 = {
+    nombre: "Audífonos alámbricos PS4",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/D_NQ_NP_742938-MCO40052446796_122019-O.webp?alt=media&token=af451703-4530-4179-b673-28d011c603b5",
+    estado: "Nuevo",
+    precio: 15000
+}
+let producto11 = {
+    nombre: "Cable cargador DualShock 4",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/D_NQ_NP_797194-MCO43412176402_092020-O.jpg?alt=media&token=e3655d92-f730-41b9-a610-dce877ccc4a2",
+    estado: "Nuevo",
+    precio: 15000
+}
+let producto12 = {
+    nombre: "DualSense",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/PS5-DualSense-MidnightBlack.jpg?alt=media&token=facdaf23-b00b-4ffa-a890-782f057025e7",
+    estado: "Nuevo",
+    precio: 225000
+}
+let producto13 = {
+    nombre: "Estuche silicona DualShock 4",
+    capacidad: "1 Control",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/bdedc49a1ea3bc7f7a643b519c5bedf8-product.jpg?alt=media&token=9a0e98f9-91fe-4ad6-88e1-0faf367f0e00",
+    estado: "Nuevo",
+    precio: 7500
+}
+let producto14 = {
+    nombre: "Mouse",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/g502-lightspeed-hero.png?alt=media&token=dae69214-511f-490f-bff0-6c020ef3996a",
+    estado: "Nuevo",
+    precio: 180000
+}
+let producto15 = {
+    nombre: "Televisor",
+    capacidad: "NaN",
+    foto: "https://firebasestorage.googleapis.com/v0/b/clinicadelplay-cf1d6.appspot.com/o/tv.jpg?alt=media&token=fe2924bb-d1ba-4bbf-b4f9-13846245fd33",
+    estado: "Nuevo",
+    precio: 250000
+}
+
+let productos = Array(
+    producto1,
+    producto2,
+    producto3,
+    producto4,
+    producto5,
+    producto6,
+    producto7,
+    producto8,
+    producto9,
+    producto10,
+    producto11,
+    producto12,
+    producto13,
+    producto14,
+    producto15
+)
+console.log(productos)
+
+productos.forEach(function(producto){
+    console.log(producto.nombre)
+})
